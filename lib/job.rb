077f91ee156e300ca77e96bf921028f2cafded1c
Brian Groat <Barnum7@users.noreply.github.com>
2015-10-28 15:17:37 -0400

    Successfully created functionality whereby the base price is marked up by 5%

--- lib/job.rb
+++ lib/job.rb
@@ -5,4 +5,16 @@ class Job
 		@people = people
 		@materials = materials
 	end
+
+	public
+
+	def initial_markup
+		starting_price = @base_price * 1.05
+		puts starting_price
+	end
+
+	job = Job.new(10,1,'bats')
+
+	job.initial_markup 
+
 end
