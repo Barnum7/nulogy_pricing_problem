6729acc16574add633e2cefe28ba8affd5474bdc
Brian Groat <Barnum7@users.noreply.github.com>
2015-10-28 16:09:33 -0400

    IT WORKSgit status Now I need to write a command line application where someone can input the paramaters and get new data

--- lib/job.rb
+++ lib/job.rb
@@ -8,17 +8,37 @@ class Job
 
 	public
 
-	def initial_markup
+	# def initial_markup
+	# 	initial_markup = @base_price * 1.05
+	# 	puts initial_markup
+	# end
+
+	# def people_markup
+	# 	people_markup = @people * 0.012
+	# 	puts people_markup
+	# end
+
+	# def materials_markup
+	# 	case material = @materials
+	# 	when 'drugs'
+	# 		materials_markup = 0.075
+	# 	when 'food'
+	# 		materials_markup = 0.13
+	# 	when 'electronics'
+	# 		materials_markup = 0.02
+	# 	else 
+	# 		materials_markup = 0
+	# 	end
+
+	# 	puts materials_markup
+	# end
+
+	def final_price
+
 		initial_markup = @base_price * 1.05
-		puts initial_markup
-	end
 
-	def people_markup
 		people_markup = @people * 0.012
-		puts people_markup
-	end
 
-	def materials_markup
 		case material = @materials
 		when 'drugs'
 			materials_markup = 0.075
@@ -30,18 +50,18 @@ class Job
 			materials_markup = 0
 		end
 
-		puts materials_markup
-	end
-
-	def final_price
 		additional_markup = people_markup + materials_markup + 1
 		final_price = initial_markup * additional_markup
 
-		puts final_price
+		puts final_price.round(2)
 	end
 
-	job = Job.new(1299.99,3,'food')
+	job = Job.new(1299.99, 3,'food')
+	job2 = Job.new(5432.00, 1, 'drugs')
+	job3 = Job.new(12456.95, 4, 'books')
 
 	job.final_price
-
+	job2.final_price
+	job3.final_price
+	
 end
